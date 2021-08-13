97fdb8187811808cd960f405aeb2bc79643c3acb
unknown <hemanth.ks97@gmail.com>
2021-08-13 16:41:41 +0530

    Made a CLI interface to generate the random entries

--- consumer.py
+++ consumer.py
@@ -1,6 +1,21 @@
+import os
 from DatabaseHandler import DatabaseHandler
 
+#Creating database handler class
 dbh = DatabaseHandler()
-dbh.RetrieveData(0)
-dbh.RetrieveData(1)
+
+#Check if the database file exists and create the database
+if (os.path.isfile(dbh.DBPath) == False):
+    dbh.RetrieveData(0)
+    dbh.RetrieveData(1) 
+
+operation = input("Enter 1 to start the essay session: ")
+if(operation == "1"):
+    issueTask = dbh.PullRandom(0)
+    argumentTask = dbh.PullRandom(1)
+    print("###########################################################")
+    print(issueTask)
+    print("###########################################################")
+    print(argumentTask)
+
 dbh.CloseDb()
